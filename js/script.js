9717e364e6def6a28d1a4b33e424bb51a0244f23
Frost <cpaccione77@gmail.com>
2018-01-19 11:00:42 -0600

    CSS updates

--- js/script.js
+++ js/script.js
@@ -1,19 +1,45 @@
   $(document).ready(function($) {
 
+  // Smooth Scroll
+  // Add smooth scrolling to all links
+  $("a.smooth").on('click', function(event) {
 
-// Defining a function to set size for #hero
- function fullscreen(){
-   $('.home-hero').css({
-           width: $(window).width(),
-           height: $(window).height()
-       });
- }
- // call function
- fullscreen();
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
- // Run the function in case of window resize
- $(window).resize(function() {
-      fullscreen();
-   });
+      // Store hash
+      var hash = this.hash;
 
-   });
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  // Responsive images
+  $('img').addClass('img-fluid');
+
+  // Defining a function to set size for #hero
+   function fullscreen(){
+     $('.home-hero').css({
+             width: $(window).width(),
+             height: $(window).height()
+         });
+   }
+   // call function
+   fullscreen();
+
+   // Run the function in case of window resize
+   $(window).resize(function() {
+        fullscreen();
+    });
+
+  });
